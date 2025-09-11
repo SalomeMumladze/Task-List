9d34dc7cd5a69d02a7eb7d909498c5aceb3475dd
salomeMumladze <salomemumladze775@gmail.com>
2025-09-11 11:59:12 +0400

    add old method

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -17,13 +17,17 @@
     its happens when a malicious website or script will send a request to a different 
     website on  behalf of the logged in user. So this essentially means that someone 
     tries to send a request on behalf of you basically to this website,and it just happens when the user
-    has an active session on the website. And to protect against such attacks, Laravel has the Csrf middleware. --}}
+    has an active session on the website. And to protect against such attacks, Laravel has the Csrf middleware.
+    
+    Remember old() only works with the method post.
+
+    --}}
     @csrf 
     <div>
       <label for="title">
         Title
       </label>
-      <input text="text" name="title" id="title" />
+      <input text="text" name="title" id="title"  value="{{old('title')}}"/>
      @error('title')
         <p class="error-message">{{ $message }}</p>
       @enderror
@@ -31,7 +35,7 @@
 
     <div>
       <label for="description">Description</label>
-      <textarea name="description" id="description" rows="5"></textarea>
+      <textarea name="description" id="description" rows="5">{{old('description')}}</textarea>
       @error('description')
         <p class="error-message">{{$message}}</p>
       @enderror
@@ -39,7 +43,7 @@
 
     <div>
       <label for="long_description">Long Description</label>
-      <textarea name="long_description" id="long_description" rows="10"></textarea>
+      <textarea name="long_description" id="long_description" rows="10">{{old('long_description')}}</textarea>
        @error('long_description')
       <p class="error-message">{{$message}}</p>
       @enderror
