e68d9c85798e717296a1e13734e2e0354e1b1c96
salomeMumladze <salomemumladze775@gmail.com>
2025-09-11 11:32:08 +0400

    add session

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Models\Task;
 
@@ -19,10 +20,28 @@ Route::get('/tasks', function(){
     ]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create')->name('create');
+
 Route::get('/tasks/{id}', function($id){
     return view('show', ['task'=> Task::findorFail($id)]);
     
 })->name('tasks.show');
+
+Route::post('/tasks', function(Request $request){
+   $data = $request->validate([
+        'title'=>'required|max:255',
+        'description'=>'required',
+        'long_description'=>'required',
+   ]);
+
+   $task = new Task;
+   $task->title=$data['title'];
+   $task->description=$data['description'];
+   $task->long_description=$data['long_description'];
+   $task -> save();
+
+   return redirect()->route('tasks.show', ['id'=> $task->id])->with('success', 'Task created successfully!');
+})->name('tasks.store');
 
 //post - fecth data
 //put - modifie
