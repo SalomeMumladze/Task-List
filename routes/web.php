cbd59f498b1a9391269eb8be09ad19066e51991c
salomeMumladze <salomemumladze775@gmail.com>
2025-09-11 12:25:53 +0400

    add tasks request

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Http\Requests\TaskRequest;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -33,36 +33,14 @@ Route::get('/tasks/{id}/edit', function ($id) {
 })->name('tasks.edit');
 
 
-Route::post('/tasks', function(Request $request){
-   $data = $request->validate([
-        'title'=>'required|max:255',
-        'description'=>'required',
-        'long_description'=>'required',
-   ]);
-
-   $task = new Task;
-   $task->title=$data['title'];
-   $task->description=$data['description'];
-   $task->long_description=$data['long_description'];
-   $task -> save();
-
+Route::post('/tasks', function(TaskRequest $request){
+   $task =  Task::create($request->validated());
    return redirect()->route('tasks.show', ['id'=> $task->id])->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
 
-Route::put('/tasks/{id}', function ($id, Request $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required'
-    ]);
-
-    $task = Task::findOrFail($id);
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
-
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+    $task->update($request->validated());
     return redirect()->route('tasks.show', ['id' => $task->id])
         ->with('success', 'Task updated successfully!');
 })->name('tasks.update');
